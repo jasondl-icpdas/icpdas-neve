3e5304f892534e601a08ee06ea4492fbd0be94c0
Jason-Kyle De Lara <jason8oh8@gmail.com>
2022-02-09 13:13:38 -0800

    testing theme update

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -22,7 +22,7 @@ global $product;
 // echo "<pre>";
 // var_dump($product->get_data());
 // echo "</pre>";
-echo "hello";
+// echo "hello";
 
 // Ensure visibility.
 if ( empty( $product ) || ! $product->is_visible() ) {
