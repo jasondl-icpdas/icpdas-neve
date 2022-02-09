508c1d112b9b000108eb7f58f67e2be31262a82e
Jason-Kyle De Lara <jason8oh8@gmail.com>
2022-02-09 13:51:52 -0800

    Quickfix

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -42,41 +42,17 @@ $product_data = $product->get_data();
         </div>
         <p class="font-thin"><?= $product_data['short_description']; ?></p>
         <hr>
-        <div class="flex flex-wrap mt-12 justify-center">
-            <div class="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-6 lg:grid-cols-6 xl:grid-cols-6 gap-4">
-                <div class="col-span-2 sm:col-span-1 xl:col-span-1">
-                    <img alt="..." src="https://source.unsplash.com/gUBJ9vSlky0" class="h-24 w-24 rounded  mx-auto" />
-                </div>
-                <div class="col-span-2 sm:col-span-4 xl:col-span-4">
-                    <h3 class="font-semibold text-black">Veggie</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </p>
-                </div>
-                <div class="col-span-2 sm:col-span-1 xl:col-span-1 italic ">$22</div>
-
-                <div class="col-span-2 sm:col-span-1 xl:col-span-1">
-                    <img alt="..." src="https://source.unsplash.com/UxRhrU8fPHQ" class="h-24 w-24 rounded  mx-auto" />
-                </div>
-                <div class="col-span-2 sm:col-span-4 xl:col-span-4">
-                    <h3 class="font-semibold text-black">Festival</h3>
-                    <p>
-                        Nam ac ligula in metus feugiat pulvinar vel ac augue.
-                    </p>
-                </div>
-                <div class="col-span-2 sm:col-span-1 xl:col-span-1 italic ">$16</div>
-
-                <div class="col-span-2 sm:col-span-1 xl:col-span-1">
-                    <img alt="..." src="https://source.unsplash.com/uU0Anw-8Vsg" class="h-24 w-24 rounded  mx-auto" />
-                </div>
-                <div class="col-span-2 sm:col-span-4 xl:col-span-4">
-                    <h3 class="font-semibold text-black">Pepperoni</h3>
-                    <p>
-                        Suspendisse pharetra lacus in ipsum porta rutrum.
-                    </p>
-                </div>
-                <div class="col-span-2 sm:col-span-1 xl:col-span-1 italic ">$18</div>
+        <div>
+            <div class="col-span-2 sm:col-span-1 xl:col-span-1">
+                <img alt="..." src="https://source.unsplash.com/gUBJ9vSlky0" class="h-24 w-24 rounded  mx-auto" />
             </div>
+            <div class="col-span-2 sm:col-span-4 xl:col-span-4">
+                <h3 class="font-semibold text-black">Veggie</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+            <div class="col-span-2 sm:col-span-1 xl:col-span-1 italic ">$22</div>
         </div>
         <?php
 
